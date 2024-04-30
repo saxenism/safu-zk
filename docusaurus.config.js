d5fa373a605eba0e338d90b89e6a5808d526e812
saxenism <rahulsaxena.pro@gmail.com>
2024-04-30 13:15:43 +0530

    asdsa

--- docusaurus.config.js
+++ docusaurus.config.js
@@ -13,10 +13,10 @@ const config = {
   favicon: 'img/favicon.ico',
 
   // Set the production url of your site here
-  url: 'https://safuzk.com/',
+  url: 'https://safuzk.com',
   // Set the /<baseUrl>/ pathname under which your site is served
   // For GitHub pages deployment, it is often '/<projectName>/'
-  baseUrl: '/safu-zk',
+  baseUrl: '/',
 
   // GitHub pages deployment config.
   // If you aren't using GitHub pages, you don't need these.
